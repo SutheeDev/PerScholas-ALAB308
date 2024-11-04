fe8255ad1a94167a23af126fab7f2622c641835b
SU-T <guitaristpeet@gmail.com>
2024-11-04 10:06:13 -0500

    part1 implementation

--- index.js
+++ index.js
@@ -6,52 +6,58 @@ const n2 = 15;
 const n3 = 20;
 const n4 = 5;
 
-// Check one: add up to 50
-// This is a fairly simple operation using
-// arithmetic operators and a comparison.
-const isSum50 = n1 + n2 + n3 + n4 == 50;
+// Implementation of Part 1
+// 1. Check if all numbers are divisible by 5
+const divideByFive = (num) => {
+  return num % 5 === 0;
+};
+const isN1DivisibleBy5 = divideByFive(n1);
+const isN2DivisibleBy5 = divideByFive(n2);
+const isN3DivisibleBy5 = divideByFive(n3);
+const isN4DivisibleBy5 = divideByFive(n4);
+console.log(
+  `Check if ${n1} is divisible by 5 and the answer is ${isN1DivisibleBy5}`
+);
+console.log(
+  `Check if ${n2} is divisible by 5 and the answer is ${isN2DivisibleBy5}`
+);
+console.log(
+  `Check if ${n3} is divisible by 5 and the answer is ${isN3DivisibleBy5}`
+);
+console.log(
+  `Check if ${n4} is divisible by 5 and the answer is ${isN4DivisibleBy5}`
+);
 
-// Check two: at least two odd numbers
-// Here, we use modulus to check if something is odd.
-// Since % 2 is 0 if even and 1 if odd, we can use
-// arithmetic to count the total number of odd numbers.
-const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+// 2. Check if the first number is larger than the last
+const isFirstLargerThanLast = n1 > n4;
+console.log(
+  `Check if ${n1} is larger than ${n4} and the answer is ${isFirstLargerThanLast}`
+);
 
-// Check three: no number larger than 25
-// This time, we use the OR operator to check
-// if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+// 3. Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+// Multiply the result by the third number.
+// Find the remainder of dividing the result by the fourth number.
 
-// Check four: all unique numbers
-// This is long, and there are more efficient
-// ways of handling it with other data structures
-// that we will review later.
-const isUnique =
-  n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+const arithmeticChain = (n1, n2, n3, n4) => {
+  let result = ((n1 - n2) * n3) % n4;
+  return result;
+};
+let outcome = arithmeticChain(n1, n2, n3, n4);
+console.log(`The result of this arithmetic chain is ${outcome}`);
 
-// Here, we put the results into a single variable
-// for convenience. Note how we negate isOver25 using
-// the ! operator. We could also have tested for
-// "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
-
-// Finally, log the results.
-console.log(isValid);
-
-// Here's another example of how this COULD be done,
-// but it SHOULD NOT be done this way. As programmers,
-// we break things into small, manageable pieces so that
-// they can be better understood, scaled, and maintained.
-const dontDoThis =
-  n1 + n2 + n3 + n4 == 50 &&
-  (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2 &&
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) &&
-  n1 != n2 &&
-  n1 != n3 &&
-  n1 != n4 &&
-  n2 != n3 &&
-  n2 != n4 &&
-  n3 != n4;
+// 4. Change the way that isOver25 calculates
+const isOver25 = (num) => {
+  return num > 25;
+};
+const isn1Over25 = isOver25(n1);
+const isn2Over25 = isOver25(n2);
+const isn3Over25 = isOver25(n3);
+const isn4Over25 = isOver25(n4);
+console.log(`Check if ${n1} is over 25 and the answer is ${isn1Over25}`);
+console.log(`Check if ${n2} is over 25 and the answer is ${isn2Over25}`);
+console.log(`Check if ${n3} is over 25 and the answer is ${isn3Over25}`);
+console.log(`Check if ${n4} is over 25 and the answer is ${isn4Over25}`);
 
 // Part 2: Practical Math
 
